f8a4267379bd81f7d95d853f7e620da1811a0385
FileOfName <81038683+FileOfName@users.noreply.github.com>
2023-09-22 22:37:01 +0200

    Fix formatting

--- src/main/java/com/fileofname/hswf3/HSWF3Mod.java
+++ src/main/java/com/fileofname/hswf3/HSWF3Mod.java
@@ -7,7 +7,7 @@ import net.fabricmc.api.ClientModInitializer;
 
 //Currently unused but might be later
 public class HSWF3Mod implements ClientModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("hswf3");
+	public static final Logger LOGGER = LoggerFactory.getLogger("hswf3");
 
 	@Override
 	public void onInitializeClient() {
